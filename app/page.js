9129c2871f133696ce338e458ffa8a0b645cfe2c
Ben Johnson <monk.sound.design@gmail.com>
2023-10-09 18:19:21 -0400

    style tweaks

--- app/page.js
+++ app/page.js
@@ -5,10 +5,12 @@ import { buttonText } from "./constants";
 export default function Home() {
   return (
     <main>
-      <div className="bg-abella-hero bg-cover bg-no-repeat bg-center">
-        <Logo />
-        <PrimaryButton>{buttonText.schedule}</PrimaryButton>
-      </div>
+      <section id="hero" className="h-screen w-full">
+        <div className="bg-abella-hero bg-cover bg-no-repeat bg-center">
+          <Logo />
+          <PrimaryButton>{buttonText.schedule}</PrimaryButton>
+        </div>
+      </section>
     </main>
   );
 }
