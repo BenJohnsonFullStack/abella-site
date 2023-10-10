d6ca8359268fc123beca0ff4b1504ba524f0a311
Ben Johnson <monk.sound.design@gmail.com>
2023-10-09 23:08:54 -0400

    button spacing and shadow

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,7 +4,7 @@ import { buttonText } from "../constants";
 const Hero = () => {
   return (
     <section id="hero" className="w-full h-screen mx-auto">
-      <div className="h-screen mx-auto flex flex-col items-center gap-20">
+      <div className="h-screen mx-auto flex flex-col items-center justify-between border-solid border-2 border-red-600">
         <Logo />
         <PrimaryButton>{buttonText.schedule}</PrimaryButton>
       </div>
--- app/components/PrimaryButton.jsx
+++ app/components/PrimaryButton.jsx
@@ -1,7 +1,7 @@
 const PrimaryButton = ({ children }) => {
   return (
     <div className="text-center pb-14">
-      <button className="bg-[#24166d] mb-16 py-2 px-12 rounded-lg text-[#edece6] drop-shadow-md border-solid border-2 border-yellow-300">
+      <button className="bg-[#24166d] py-2 px-12 rounded-lg text-[#edece6] drop-shadow-md border-solid border-2 border-yellow-300 shadow-xl">
         {children}
       </button>
     </div>
