59a6ce63e7be4312ce9d4309ad3f2f94c563c650
Ben Johnson <monk.sound.design@gmail.com>
2023-10-09 23:14:15 -0400

    remove temp red border

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,7 +4,7 @@ import { buttonText } from "../constants";
 const Hero = () => {
   return (
     <section id="hero" className="w-full h-screen mx-auto">
-      <div className="h-screen mx-auto flex flex-col items-center justify-between border-solid border-2 border-red-600">
+      <div className="h-screen mx-auto flex flex-col items-center justify-between">
         <Logo />
         <PrimaryButton>{buttonText.schedule}</PrimaryButton>
       </div>
