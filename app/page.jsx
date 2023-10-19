f404a6bb8d882c73275fd079ab3a50ae99dd9c59
Ben Johnson <monk.sound.design@gmail.com>
2023-10-19 16:19:07 -0400

    syntax cleanup

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,7 @@ import { Hero } from "../components/index";
 export default function Home() {
   return (
     <main>
-      <div>
-        <Hero />
-      </div>
+      <Hero />
     </main>
   );
 }
