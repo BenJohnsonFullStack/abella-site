a4bba772cb90b64caa33051f338f242dfa44df63
Ben Johnson <monk.sound.design@gmail.com>
2023-10-24 14:55:55 -0400

    address in footer

--- components/sections/Footer.jsx
+++ components/sections/Footer.jsx
@@ -3,7 +3,22 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <section id="footer">Footer</section>
+      <section id="footer">
+        <div className="footer-container">
+          <h3>Questions? Comments?</h3>
+          <address className="address">
+            889 Peachtree Dr
+            <br />
+            Columbus, GA 31906
+            <br />
+            <a href="tel:7067806827">706.780.6827</a>
+            <br />
+            <a href="email:contact@abella-health.com">
+              contact@abella-health.com
+            </a>
+          </address>
+        </div>
+      </section>
     </>
   );
 };
