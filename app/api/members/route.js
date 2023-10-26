4d10def06a329bf2ae2e20edb3419feeb36f5a27
Ben Johnson <monk.sound.design@gmail.com>
2023-10-25 21:02:57 -0400

    commented out old Subscriber imports to eliminate errors (no longer in use but might come back

--- app/api/members/route.js
+++ app/api/members/route.js
@@ -1,9 +1,9 @@
-import { Subscriber } from "@/models";
+// import { Subscriber } from "@/models";
 
 export const POST = async (req, res) => {
   try {
     const newSubscriber = req.body();
-    await Subscriber.addSubscriber(newSubscriber);
+    // await Subscriber.addSubscriber(newSubscriber);
     res.status(201).json({
       message:
         "Thanks for your submission! Someone from our team will reach out to you as soon as possible.",
