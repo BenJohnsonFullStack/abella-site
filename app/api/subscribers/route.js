7ebb19688b8c71e5125fe7985520d5d02c07278d
Ben Johnson <monk.sound.design@gmail.com>
2023-10-25 22:39:08 -0400

    api/subscribers POST endpoint wired

---
+++ app/api/subscribers/route.js
@@ -0,0 +1,27 @@
+// import { Subscriber } from "@/models";
+import { prisma } from "@/lib/prisma";
+
+export const POST = async (req) => {
+  try {
+    // store newSubscriber in variable
+    const newSubscriber = await req.json();
+
+    // access Subscriber model
+    const subscriber = await prisma.Subscriber.upsert({
+      // check if email exists
+      where: {
+        email: newSubscriber.email,
+      },
+      // if exists, do nothing
+      update: {},
+      // otherwise, create newSubscriber in database
+      create: newSubscriber,
+    });
+    // return newSubscriber and status
+    return new Response(JSON.stringify(subscriber), { status: 201 });
+  } catch (err) {
+    return new Response(JSON.stringify(err.message), {
+      status: 500,
+    });
+  }
+};
