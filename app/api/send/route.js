627b25286925c70df0343e9e410402f21bc04f59
Ben Johnson <monk.sound.design@gmail.com>
2023-11-03 01:46:42 -0400

    new Resend for confirmation email, html passing in subscriber

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -2,18 +2,38 @@ import { Resend } from "resend";
 import { Test } from "@/emails";
 
 export async function POST(req) {
-  const { first_name, email } = await req.json();
+  const subscriber = await req.json();
   const formattedName =
-    first_name.charAt(0).toUpperCase() + first_name.slice(1);
+    subscriber.first_name.charAt(0).toUpperCase() +
+    subscriber.first_name.slice(1);
 
   try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+    const htmlEmail = new Resend(process.env.RESEND_API_KEY);
 
-    const data = await resend.emails.send({
+    const data = await htmlEmail.emails.send({
       from: "Abella Total Health <contact@abella-health.com>",
-      to: `${email}`,
+      to: `${subscriber.email}`,
       subject: `Welcome, ${formattedName}`,
       react: <Test first_name={formattedName} />,
+    });
+
+    const confirmationEmail = new Resend(process.env.RESEND_API_KEY);
+
+    await confirmationEmail.emails.send({
+      from: "Abella Admin <contact@abella-health.com>",
+      to: "sherri.johnston3626@gmail.com",
+      subject: "New Member Sign-Up",
+      html: `
+      First Name: ${subscriber.first_name}<br />
+      Last Name: ${subscriber.last_name}<br />
+      Email: ${subscriber.email}<br />
+      Phone Number: ${subscriber.phone}<br />
+      Street Address: ${subscriber.street_address}<br />
+      City: ${subscriber.city}<br />
+      State: ${subscriber.state}<br />
+      Zip Code: ${subscriber.zip_code}<br />
+      Date of Birth: ${subscriber.date_of_birth}
+      `,
     });
 
     return new Response(JSON.stringify(data), { status: 201 });
